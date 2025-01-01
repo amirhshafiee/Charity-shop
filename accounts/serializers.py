473f8aab864ce5f0d6b1d527bb9abd4c088eb7dd
Amirhosien <amirshafiee266@yahoo.com>
2025-01-01 19:50:32 +0330

    Tas-1

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -4,4 +4,16 @@ from .models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        model = User
+        fields = ["username", "password", "phone", "address", "gender",
+                  "age", "description", "first_name", "last_name", "email"]
+
+    def create(self, validated_data):
+        # Hash Password
+        password = validated_data["password"]
+        user = User(**validated_data)
+        user.set_password(password)
+
+        user.save()
+        return user
--- accounts/views.py
+++ accounts/views.py
@@ -19,4 +19,10 @@ class LogoutAPIView(APIView):
 
 
 class UserRegistration(APIView):
-    pass
+    def post(self, request):
+        ser_data = UserSerializer(data=request.POST)
+        if ser_data.is_valid():
+            ser_data.save()
+            return Response(data=ser_data.data, status=status.HTTP_200_OK)
+
+        return Response(data=ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
